3886bbad1e0c889d4fbd020231b246d5132ccc08
Josh Rogen <rogenjh@gmail.com>
2020-09-11 14:14:26 -0400

    proxy-server running for deployed instances

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3030;
+const PORT = 8080;
+require('newrelic');
+const axios = require('axios');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.use('/airbnb/', express.static(path.join(__dirname, '../public')));
+app.use('/:id', express.static(path.join(__dirname, '../public')));
+
+app.use('/residences/:id', createProxyMiddleware({ target: 'http://35.168.8.22', changeOrigin: true }));
+app.use('/api/images/:id', createProxyMiddleware({ target: 'http://3.15.189.69:4001', changeOrigin: true }));
+app.use('/api/reviews/:id', createProxyMiddleware({ target: 'http://3.135.189.4:3000', changeOrigin: true }));
+app.use('/api/booking/:id', createProxyMiddleware({ target: 'http://13.56.164.217', changeOrigin: true }));
+
+app.use('/:id/fonts/', express.static('./fonts'))
 
 app.listen(PORT, () => console.log('Proxy Server listening on port ', PORT));
